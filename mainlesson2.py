44d2306a5cc68b003b7dc6f8c29343d4c4858c77
AntoninaBT <gorbaa533@gmail.com>
2024-10-12 17:14:59 +0300

    Add files via upload

---
+++ mainlesson2.py
@@ -0,0 +1,8 @@
+# Teplova // 1st program. I need to count 9^0.5*5. I enter "print()" and write the example without "" because it's integers
+print(9 ** 0.5 * 5)
+# Teplova // 2nd program. I need to check 9.99>=9.98 and 1000 doesn't equal 1001 at the same time and output the result.
+print(9.99 >= 9.98 and 1000 != 1001) #boolean - a data type
+# Teplova // 3rd program. I need to count 2*2+2, 2*(2+2) and output then compare these two results
+print(2 * 2 + 2, 2 * (2 + 2), 2 * 2 + 2 == 2 * (2 + 2))
+# Teplova // 4th program. I need to get 4 out of 123.456 using float, *, % and int. I exchange the text 123.456 to 123.456 (use "float" then multiply by 10 and use %. In the end I put "int" before "float"
+print(int(float('123.456') * 10 % 10))
